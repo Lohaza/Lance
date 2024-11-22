4d2194f6b5fc238e54a98c1b801104ae483b7219
lohaza <harrisriaz0@gmail.com>
2024-11-22 14:53:56 +0000

    JS gets better

--- create account lance.js
+++ create account lance.js
@@ -1,4 +1,22 @@
 function enter(){
-let pass=document.getElementById("pass_in");
-console.log(pass);
+    let pass=document.getElementById("pass_in").value;
+    console.log(pass);
+    let con_pass=document.getElementById("confirm_pass_in").value;
+    console.log(con_pass);
+    pass_check(con_pass,pass)
+    reset()
+}
+
+
+function reset(){
+    document.getElementById("confirm_pass_in").value="";
+    document.getElementById("pass_in").value="";
+}
+    
+
+function pass_check(con_pass,pass){
+if (pass==con_pass){
+    console.log("passwords match")
+}
+
 }
