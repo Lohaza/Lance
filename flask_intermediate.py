5c077f36a2e889eaab3d9fe74299ba2ae174151b
lohaza <harrisriaz0@gmail.com>
2025-01-08 02:26:03 +0000

    database is getting ready to be worked on

--- flask_intermediate.py
+++ flask_intermediate.py
@@ -1,9 +1,30 @@
 
 import hashlib
+import mysql.connector
 from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
 app= Flask(__name__, static_folder='static')
 CORS(app)
+
+
+
+db=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="Lance"
+    
+)
+
+mycursor=db.cursor()
+#mycursor.execute("CREATE DATABASE Lance")
+#mycursor.execute("CREATE TABLE Users(name VARCHAR(20),password VARCHAR(300), userID int PRIMARY KEY AUTO_INCREMENT)")
+#mycursor.execute("ALTER TABLE Users MODIFY password VARCHAR(255);")
+
+mycursor.execute("SELECT * FROM Users")
+
+for x in mycursor:
+    print(x)
 
 def pass_hash(user_pass):
     hash_pass = hashlib.sha256(user_pass.encode())
@@ -19,6 +40,9 @@ def create_your_account():
         user_pass=request.json["password"]
         user_email=request.json["email"]
         hashed_password=pass_hash(user_pass)
+        mycursor.execute("INSERT INTO Users (name, password) VALUES (%s, %s)", (user_name, hashed_password))
+        db.commit()
+        
 
         print("Received POST data:", user_name, user_pass, user_email)
         print("Hashed password:", hashed_password)
